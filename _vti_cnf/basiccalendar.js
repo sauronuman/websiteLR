ea2853877bf704bcc75481bb339d3cc9bab01cee
D Gartland <dgartland021@c2ken.net>
2024-02-25 10:19:02 +0000

    Initial commit of website files

---
+++ _vti_cnf/basiccalendar.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|19 Oct 2023 10:48:49 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|C2KEN\\lrice414
+vti_modifiedby:SR|C2KEN\\lrice414
+vti_timecreated:TR|19 Oct 2023 10:48:49 -0000
+vti_cacheddtm:TX|19 Oct 2023 10:48:49 -0000
+vti_filesize:IR|1254
+vti_backlinkinfo:VX|Template.dwt HTML/Meet\\ The\\ Team.html HTML/Apply\\ Here.html HTML/Courses.html HTML/Job\\ Specifications.html Index.html
